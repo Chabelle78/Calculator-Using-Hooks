a05597257b271ae3b05d7fd0a3a4f3dfaf5b07b5
CHABAT ESTELLE <chabat.estelle@gmail.com>
2021-04-15 11:26:53 +0200

    Counter

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,18 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import logo from "./logo.svg";
 import "./App.css";
 
 function App() {
   const [count, setCount] = useState(10);
 
+  useEffect(() => {
+    document.title = `Result is ${count}`;
+  });
+
   return (
     <div className="App">
       <div>CALCUL</div>
-      <p>{count}</p>
+      <p>You result {count} </p>
       <button onClick={() => setCount(count - 1)}>-</button>
       <button onClick={() => setCount(count + 1)}>+</button>
       <p>Divide by 2 or multiply by 6</p>
